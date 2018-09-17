c8b14c7652636a9915ec185c943b6fe16f21bed6
Eric <enshapir@andrew.cmu.edu>
2018-09-17 17:14:58 -0400

    Data Loads
    The data set now loads into R

--- app.R
+++ app.R
@@ -1,20 +1,25 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# ----------------------
+#enshapir
+# 
+# project 1
+#-----------------------
 
 library(shiny)
 library(tidyverse)
+library(readxl)
+library(rjson)
 library(plotly)
 library(DT)
 
-# Define dashboard UI
+#DOHMH New York City Restaurant Inspection Results
+#https://data.cityofnewyork.us/Health/DOHMH-New-York-City-Restaurant-Inspection-Results/43nn-pn8j
 
-  
+#file path for data
+Resturant.file.path <- file.path('data', 'DOHMH_New_York_City_Restaurant_Inspection_Results.xlsx', fsep = .Platform$file.sep)
+
+Resturant.load <- read_xlsx(path = Resturant.file.path, sheet = 1, col_names = TRUE)
+
+# Define dashboard UI
 header <- dashboardHeader()
 
 sidebar <- dashboardSidebar()
