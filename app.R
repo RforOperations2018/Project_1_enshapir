fb128fc8aebe735b116a14501a060de46cfc0ddd
Eric <enshapir@andrew.cmu.edu>
2018-09-21 13:50:36 -0400

    clean up code and data table
    cleaned up my code and implimented the data table

--- app.R
+++ app.R
@@ -65,76 +65,23 @@ body <- dashboardBody(tabItems(
                    tabPanel("Number of Violations Over Time", plotlyOutput("ViolationsOverTime")))
           )
   ),
+  
+    tabItem(tabName = "HPC",
+            fluidRow(
+              tabBox(title = "",
+                     width = 12,
+                     tabPanel("Number of Violations Over Time within Zip", textOutput("VioZip")),
+                     tabPanel("Number of Violations by Comparison by Cuisine", plotlyOutput("VioCuisine")))
+            )
+
+    ),
+  
   tabItem("table",
           fluidPage(
-            box(title = "Selected Character Stats", DT::dataTableOutput("table"), width = 12))
+            box(title = "Selected Resturant Data", DT::dataTableOutput("table"), width = 12))
   )
 )
 )
-
-
-# 
-# 
-# 
-# 
-# 
-# 
-# body <- dashboardBody(tabItems(
-#   tabItem("plot",
-#           fluidRow(
-#             infoBoxOutput("mass"),
-#             valueBoxOutput("height")
-#           ),
-#           fluidRow(
-#             tabBox(title = "Plot",
-#                    width = 12,
-#                    tabPanel("Mass", plotlyOutput("plot_mass")),
-#                    tabPanel("Height", plotlyOutput("plot_height")))
-#           )
-#   ),
-#   
-#   tabItem(tabName = "CP", 
-#     fluidRow(
-#       box(valueBoxOutput(outputId = "TimeSinceInspection", width = 4)),
-#       box(valueBoxOutput(outputId = "GradeLastestInspection", width = 4)),
-#       box(valueBoxOutput(outputId = "LastestViolationCount", width = 4))
-#     ),
-#     fluidRow(
-#     box(
-#       title = "Number of Violations Over Time", solidHeader = TRUE,
-#       collapsible = FALSE,
-#       plotlyOutput("ViolationsOverTime")
-#     ),
-#     box(
-#       title = "Noted Violations", solidHeader = TRUE,
-#       collapsible = FALSE,
-#       plotlyOutput("ViolationsOverTime")
-#     )
-#     )
-#   ),
-#   
-#   
-#   tabItem(tabName = "HPC", 
-#           box(
-#             title = "Number of Violations Over Time within Zip", solidHeader = TRUE,
-#             collapsible = FALSE,
-#             plotlyOutput("ViolationsOverTime", height = 250)
-#           ),
-#           box(
-#             title = "Number of Violations by Comparison by Cuisine", solidHeader = TRUE,
-#             collapsible = FALSE,
-#             plotlyOutput("ViolationsOverTime", height = 250)
-#           )
-#   ),
-#   
-#   tabItem(tabName = "table",
-#           fluidPage(
-#             box(title = "Selected Resturants Data", 
-#                 dataTableOutput("table"), width = 12))
-#   )
-# 
-#   )
-# )
 
 
  ui <- dashboardPage(header, sidebar, body)
@@ -171,35 +118,14 @@ server <- function(input, output, session=session) {
       filter(`INSPECTION DATE` == max(res$`INSPECTION DATE`))
     valueBox(subtitle = "Violation Score (Lower is Better)", value = resCurrent$SCORE, icon = icon("exclamation-triangle "), color = "green")
   })
-
   
   
   
-  
-  
-   output$GradeLastestInspection <- renderValueBox({
-    days.since.last <- as.numeric(currentDate - lastInspect)
-    valueBox(subtitle = "Days Since Last Inspection", value = 45, icon = NULL, color = "green")
+  # Data table of Resturnat Inspections
+  output$table <- DT::renderDataTable({
+    subset(resInput(), select = c(DBA,`CUISINE DESCRIPTION`, `VIOLATION CODE`, SCORE, GRADE, `INSPECTION DATE`,`INSPECTION TYPE`))
   })
-
-  # output$LastestViolationCount <- renderValueBox({
-  #   res <- resInput()
-  #   currentDate <- Sys.Date()
-  #   lastInspect <- res$`INSPECTION DATE`
-  #   days.since.last <- as.numeric(currentDate - lastInspect)
-  #   valueBox(subtitle = "Days Since Last Inspection", value = 57, icon = NULL, color = "green")
-  # })
-
-  output$LastestViolationCount <- renderValueBox({
-    sw <- swInput()
-    num <- round(mean(sw$height, na.rm = T), 2)
-
-    valueBox(subtitle = "Avg Height", value = num, icon = icon("sort-numeric-asc"), color = "green")
-  })
-
-
-
-
+  
 }
 
 
